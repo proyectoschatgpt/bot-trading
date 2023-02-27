0270d6a85fe7e0255201eb03f6e7895dec36e47e
proyectoschatgpt <126307075+proyectoschatgpt@users.noreply.github.com>
2023-02-27 17:10:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from flask import Flask
@@ -8,7 +6,6 @@ from flask import Flask
 app = Flask(__name__)
 
 # Configuración de credenciales de Google Drive
-
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 creds = None
 
@@ -17,44 +14,47 @@ token_file = 'token.json'
 # Carga las credenciales de las credenciales de token.json
 if os.path.exists(token_file):
     creds = Credentials.from_authorized_user_file(token_file, SCOPES)
-    
+
 @app.route('/')
 def index():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
     try:
+        # Crear una instancia de la API de Google Drive
         service = build('drive', 'v3', credentials=creds)
 
-        # Call the Drive v3 API
-        results = service.files().list(
-            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        # Buscar el archivo de cuaderno de Jupyter en Google Drive
+        file_name = 'bot.ipynb' # Cambiar por el nombre de tu archivo
+        query = "mimeType='application/vnd.google-apps.document' and name='{}'".format(file_name)
+        results = service.files().list(q=query, fields='files(id)').execute()
         items = results.get('files', [])
 
+        # Verificar si se encontró el archivo
         if not items:
-            print('No files found.')
-            return
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+            return "No se encontró el archivo {}".format(file_name)
+
+        # Obtener el ID de archivo del cuaderno de Jupyter
+        file_id = items[0]['id']
+
+        # Definir los parámetros para ejecutar el cuaderno de Jupyter en Google Colab
+        params = {
+            'key': 'INSERT_API_KEY_HERE', # Reemplazar con la clave de la API de Colab
+            'includeResults': 'true'
+        }
+
+        # Definir el cuerpo de la solicitud para ejecutar el cuaderno de Jupyter en Google Colab
+        body = {
+            'mode': 'run',
+            'url': 'https://colab.research.google.com/drive/' + file_id,
+            'params': params
+        }
+
+        # Crear una instancia de la API de Google Drive para enviar la solicitud
+        colab_service = build('drive', 'v3', credentials=creds)
+        response = colab_service.files().executeFunction(fileId=file_id, body=body).execute()
+
+        return "El cuaderno de Jupyter se ha ejecutado correctamente en Google Colab."
+
     except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
-        print(f'An error occurred: {error}')
-        
+        return "Ocurrió un error: {}".format(error)
+
 if __name__ == '__main__':
     app.run()
